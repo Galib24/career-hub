1855abf97105f2709b52e1aa0ef3e9f710696826
SQLAdmin <abuyeahia24@gmail.com>
2023-04-11 17:34:27 +0600

    blogs added

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 const Blog = () => {
     return (
         <div>
-            <h2>this is blogs</h2>
+            <h2 className='text-center mb-5' style={{color: '#7E90FE'}}>This is blogs Section</h2>
+            {/* card section */}
+
+            <section className='container d-flex gap-5'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>First Question Answer</Card.Title>
+                        <Card.Text> <strong>Context API: </strong> Context is basically used when some data needs to be accessible by many components at different nesting levels, <strong>here no need to use props for rendering many time</strong>. </Card.Text>
+                    </Card.Body>
+                </Card>
+                
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Second Question Answer</Card.Title>
+                        <Card.Text> <strong>Custom Hooks: </strong> Hooks are reusable functions. When you have component logic that needs to be used by multiple components, <strong>we can extract that logic to a custom Hook. Custom Hooks start with "use"</strong>. Example: useFetch. </Card.Text>
+                    </Card.Body>
+                </Card>
+                
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Third Question Answer</Card.Title>
+                        <Card.Text> <strong>useRef: </strong> useRef(initialValue) is a built-in React hook that accepts one argument as the initial value and returns a reference (aka ref). </Card.Text>
+                    </Card.Body>
+                </Card>
+            
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Fourth Question Answer</Card.Title>
+                        <Card.Text> <strong>useMemo(): </strong> It is a function that returns a memoized value of a passed in resource-intensive function. It is very useful in optimizing the performance of a React component by eliminating repeating heavy computations </Card.Text>
+                    </Card.Body>
+                </Card>
+
+            </section>
         </div>
     );
 };
--- src/Components/View Dtalis/ViewDetails.jsx
+++ src/Components/View Dtalis/ViewDetails.jsx
@@ -1,67 +1,68 @@
 import React, { useEffect, useState } from 'react';
-import {  Button, Card } from 'react-bootstrap';
-import {  useLoaderData, useParams } from 'react-router-dom';
+import { Button, Card } from 'react-bootstrap';
+import { useLoaderData, useParams } from 'react-router-dom';
 
 
 const ViewDetails = () => {
-    const {detailsId} = useParams();
+    const { detailsId } = useParams();
     // console.log(dynamic.detailsId);
     // const [details,setDetails] =  useState([]);
     // useEffect(()=>{
     //     fetch("/jobDetails.json")
     //     .then(res => res.json())
     //     .then(data => setDetails(data))
-        
+
     // })
     const details = useLoaderData();
     // console.log(details);
-    const [jobDetails, setJobDetails]  = useState({})
-    useEffect(()=>{
-        if(details){
+    const [jobDetails, setJobDetails] = useState({})
+    useEffect(() => {
+        if (details) {
             const detailsData = details.find(dt => dt.id == detailsId);
             setJobDetails(detailsData);
         }
-    },[])
+    }, [])
     return (
         <div>
             <div className='text-center mt-5'>
-            <h2>Job Details</h2>
+                <h2>Job Details</h2>
             </div>
             <div className='d-flex gap-5 mt-5'>
                 {/* {jobDetails.Phone} */}
-<div>
-<Card>
-            
-            <Card.Body>
-              <p><small><strong>Job Description: </strong> {jobDetails.JobDescription}</small></p>
-              <br />
-              <p><small><strong>Job Responsibility: </strong> {jobDetails.JobResponsibility}</small></p>
-              <p><small><strong>Educational Requirements : <br /> </strong> {jobDetails.EducationalRequirements}</small></p>
-              <p><small><strong>Experiences: <br /> </strong> {jobDetails.Experiences}</small></p>
+                <div>
+                    <Card>
 
-            </Card.Body>
-          </Card>
-</div>
-<div>
-<Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Job Details</Card.Title>
-        {/* <Card.Text></Card.Text>  */}
-        <p><small><strong>Salary: </strong> {jobDetails.salary}</small></p>
-        <p><small><strong>Job Title: </strong> {jobDetails.jobTitle}</small></p>
-        <Card.Title>Contact Information</Card.Title>
-        <p><small><strong>Email: </strong> {jobDetails.email}</small></p>
-        <p><small><strong>Address: </strong> {jobDetails.location}</small></p>
-        <br />
+                        <Card.Body>
+                            <p><small><strong>Job Description: </strong> {jobDetails.JobDescription}</small></p>
+                            <br />
+                            <p><small><strong>Job Responsibility: </strong> {jobDetails.JobResponsibility}</small></p>
+                            <p><small><strong>Educational Requirements : <br /> </strong> {jobDetails.EducationalRequirements}</small></p>
+                            <p><small><strong>Experiences: <br /> </strong> {jobDetails.Experiences}</small></p>
 
-<Button  style={{backgroundColor: '#7E90FE'}} className='mx-4'>Apply Now</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Title>Job Details</Card.Title>
+                            {/* <Card.Text></Card.Text>  */}
+                            <p><small><strong>Salary: </strong> {jobDetails.salary}</small></p>
+                            <p><small><strong>Job Title: </strong> {jobDetails.jobTitle}</small></p>
+                            <Card.Title>Contact Information</Card.Title>
+                            <p><small><strong>Phone: </strong> {jobDetails.Phone}</small></p>
+                            <p><small><strong>Email: </strong> {jobDetails.email}</small></p>
+                            <p><small><strong>Address: </strong> {jobDetails.location}</small></p>
+                            <br />
 
-      </Card.Body>
-    </Card>
-</div>
-                
+                            <Button style={{ backgroundColor: '#7E90FE' }} className='mx-4'>Apply Now</Button>
+
+                        </Card.Body>
+                    </Card>
+                </div>
+
             </div>
-        
+
         </div>
     );
 };
